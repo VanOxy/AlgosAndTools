34aa54514e77b43fde833c73cfad514ae6651e91
DESKTOP-PBOLLA9\boss <ivanokulichev@gmail.com>
2022-02-22 03:03:28 +0100

    anagrams

---
+++ AlgosAndTools/Anagrams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgosAndTools
+{
+    internal class Anagrams
+    {
+        private static bool AreAnagrams(string str1, string str2)
+        {
+            // string str1 = "dusty", str2 = "study";
+
+            if (str1.Length != str2.Length)
+                return false;
+
+            var result = string.Concat(str1.OrderBy(x => x)) == string.Concat(str2.OrderBy(x => x)) ? "true" : "false";
+            Console.WriteLine(result);
+            return bool.Parse(result);
+        }
+    }
+}
